ab996d2b969a307fbdb45a84e521d2a8e3979d4b
Moshe Immerman <moshe@flanksource.com>
2021-08-13 12:41:42 +0200

    fix contact form

--- v2/src/components/Contact/index.js
+++ v2/src/components/Contact/index.js
@@ -111,7 +111,8 @@ export default function Contact() {
         </div>
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
-            <form className="grid grid-cols-1 gap-y-6" name="contact" method="post" data-netlify="true">
+            <form className="grid grid-cols-1 gap-y-6">
+              <input type="hidden" name="form-name" value="contact" />
               <div>
                 <label htmlFor="full-name" className="sr-only">
                   Name
