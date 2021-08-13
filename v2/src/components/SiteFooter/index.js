f4c9504d97bc9f8397f25662e159bc4863265ac6
John Goh <john@flanksource.com>
2021-08-13 13:45:15 +0800

    fix: refactor Site Footer structure

--- v2/src/components/SiteFooter/index.js
+++ v2/src/components/SiteFooter/index.js
@@ -1,189 +1,155 @@
-import { routes } from "../../routes";
-import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/images/flanksource-white.svg";
-import { FaSlack, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function SiteFooter() {
-
+  const social = [
+    {
+      name: "GitHub",
+      href: "https://www.github.com/flanksource",
+      icon: FaGithub
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/company/flanksource",
+      icon: FaLinkedin
+    }
+  ];
   const footerNavigation = {
-    kubernetes: [
-      { name: 'Quickstart', href: '/kubernetes-services#quick-start' },
-      { name: 'Managed Services', href: '/kubernetes-services#managed-services' },
-      { name: 'Operator Development', href: '/kubernetes-services#operators' },
-      { name: 'Internal Platforms', href: '/kubernetes-services#internal-platform-development' },
-      { name: 'Strategy', href: '/kubernetes-services#cloud-and-devops' },
-      { name: 'Resilience Reviews', href: '/kubernetes-services#resilience-assessment' },
-    ],
-
-    devops: [
-      { name: 'Quickstart', href: '/kubernetes-services#quick-start' },
-      { name: 'Managed Services', href: '/kubernetes-services#managed-services' },
-    ],
-
-    support: [
-      { name: 'Pricing', href: '#' },
-      { name: 'Documentation', href: '#' },
-      { name: 'Guides', href: '#' },
-      { name: 'API Status', href: '#' },
-    ],
-    opensource: [
-      { name: 'Karina', href: 'https://www.github.com/flanksource/karina' },
-      { name: 'Canary Checker', href: 'https://www.github.com/flanksource/canary-checker' },
-      { name: 'Template Operator', href: 'https://www.github.com/flanksource/template-operator' },
-      { name: 'Git Operator', href: 'https://www.github.com/flanksource/git-operator' },
-      { name: 'Platform Operator', href: 'https://www.github.com/flanksource/platform-operator' },
-      { name: 'kommons', href: 'https://www.github.com/flanksource/kommons' },
-      { name: 'konfigadm', href: 'https://www.github.com/flanksource/konfigadm' },
-    ],
-
-
-    company: [
-      { name: 'About', href: '/about' },
-      { name: 'Blog', href: '/blog' },
-      { name: 'Careers', href: '/careers' },
-    ],
-    legal: [
-      { name: 'Privacy', href: '/privacy' },
-      { name: 'Terms', href: '/terms' },
-    ],
-    social: [
-
-
-      {
-        name: 'GitHub',
-        href: 'https://www.github.com/flanksource',
-        icon: FaGithub
-      },
-      {
-        name: "LinkedIn",
-        href: "https://www.linkedin.com/company/flanksource",
-        icon: FaLinkedin,
-      }
-
-    ],
-  }
-
-
-
-  //   const navigation = {
-  //     main: [
-  //       { name: "About", href: routes.about.path },
-  //       { name: "Services", href: routes.services.path },
-  //       { name: "Open Source", href: routes.openSource.path },
-  //       { name: "Careers", href: routes.careers.path },
-  //       { name: "Blog", href: routes.blog.path },
-  //       { name: "Contact", href: routes.contact.path }
-  //     ],
-
-  //     ]
-  // };
+    kubernetes: {
+      title: "Kubernetes",
+      links: [
+        { name: "Quickstart", href: "/kubernetes-services#quick-start" },
+        {
+          name: "Managed Services",
+          href: "/kubernetes-services#managed-services"
+        },
+        {
+          name: "Operator Development",
+          href: "/kubernetes-services#operators"
+        },
+        {
+          name: "Internal Platforms",
+          href: "/kubernetes-services#internal-platform-development"
+        },
+        { name: "Strategy", href: "/kubernetes-services#cloud-and-devops" },
+        {
+          name: "Resilience Reviews",
+          href: "/kubernetes-services#resilience-assessment"
+        }
+      ]
+    },
+    devops: {
+      title: "Cloud & Devops",
+      links: [
+        { name: "Quickstart", href: "/kubernetes-services#quick-start" },
+        {
+          name: "Managed Services",
+          href: "/kubernetes-services#managed-services"
+        }
+      ]
+    },
+    // support: {
+    //   title: "Support",
+    //   links: [
+    //     { name: "Pricing", href: "#" },
+    //     { name: "Documentation", href: "#" },
+    //     { name: "Guides", href: "#" },
+    //     { name: "API Status", href: "#" }
+    //   ]
+    // },
+    opensource: {
+      title: "Open Source",
+      links: [
+        { name: "Karina", href: "https://www.github.com/flanksource/karina" },
+        {
+          name: "Canary Checker",
+          href: "https://www.github.com/flanksource/canary-checker"
+        },
+        {
+          name: "Template Operator",
+          href: "https://www.github.com/flanksource/template-operator"
+        },
+        {
+          name: "Git Operator",
+          href: "https://www.github.com/flanksource/git-operator"
+        },
+        {
+          name: "Platform Operator",
+          href: "https://www.github.com/flanksource/platform-operator"
+        },
+        { name: "kommons", href: "https://www.github.com/flanksource/kommons" },
+        {
+          name: "konfigadm",
+          href: "https://www.github.com/flanksource/konfigadm"
+        }
+      ]
+    },
+    company: {
+      title: "Company",
+      links: [
+        { name: "About", href: "/about" },
+        { name: "Blog", href: "/blog" },
+        { name: "Careers", href: "/careers" }
+      ]
+    },
+    legal: {
+      title: "Legal",
+      links: [
+        { name: "Privacy", href: "/privacy" },
+        { name: "Terms", href: "/terms" }
+      ]
+    }
+  };
 
   return (
-
     <footer className="bg-warm-gray-900" aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-
-        <div className="xl:grid xl:grid-cols-12 xl:gap-8">
-          <div className="space-y-8 xl:col-span-4">
+      <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex flex-col items-center lg:items-start mb-12 lg:mr-24 ">
             <Logo />
-
-            <div className="flex space-x-6">
-              {footerNavigation.social.map((item) => (
-                <a key={item.name} href={item.href} className="text-warm-gray-400 hover:text-warm-gray-300">
+            <div className="mt-4 ml-1 flex space-x-6">
+              {social.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-warm-gray-400 hover:text-warm-gray-300"
+                >
                   <span className="sr-only">{item.name}</span>
                   <item.icon className="h-6 w-6" aria-hidden="true" />
                 </a>
               ))}
             </div>
           </div>
-
-          {/* <div className="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-4"> */}
-
-          <div className=" md:gap-8  xl:col-span-2">
-            <div>
-              <h3 className="text-sm font-semibold text-warm-gray-200 tracking-wider uppercase">Kubernetes</h3>
-              <ul className="mt-4 space-y-4">
-                {footerNavigation.kubernetes.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className="text-base text-warm-gray-400 hover:text-warm-gray-300">
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+          <div className="w-full flex flex-col justify-between md:flex-row lg:mt-2">
+            {Object.keys(footerNavigation).map((key) => {
+              const column = footerNavigation[key];
+              return (
+                <div className="flex flex-grow ml-8 sm:ml-16 md:ml-0 mb-14 md:mb-0">
+                  <div className="md:mt-0">
+                    <h3 className="text-sm font-semibold text-warm-gray-200 tracking-wider uppercase">
+                      {column.title}
+                    </h3>
+                    <ul className="mt-4 space-y-4">
+                      {column.links.map((item) => (
+                        <li key={item.name}>
+                          <a
+                            href={item.href}
+                            className="text-base text-warm-gray-400 hover:text-warm-gray-300"
+                          >
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-
-          <div className=" md:gap-8  xl:col-span-2">
-            <div>
-              <h3 className="text-sm font-semibold text-warm-gray-200 tracking-wider uppercase">Cloud & DevOps</h3>
-              <ul className="mt-4 space-y-4">
-                {footerNavigation.devops.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className="text-base text-warm-gray-400 hover:text-warm-gray-300">
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
-
-          <div className="md:gap-8  xl:col-span-2">
-            <div className="mt-12 md:mt-0">
-              <h3 className="text-sm font-semibold text-warm-gray-200 tracking-wider uppercase">Open Source</h3>
-              <ul className="mt-4 space-y-4">
-                {footerNavigation.opensource.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className="text-base text-warm-gray-400 hover:text-warm-gray-300">
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
-
-
-
-          <div className=" md:gap-8">
-            <div>
-              <h3 className="text-sm font-semibold text-warm-gray-200 tracking-wider uppercase">Company</h3>
-              <ul className="mt-4 space-y-4">
-                {footerNavigation.company.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className="text-base text-warm-gray-400 hover:text-warm-gray-300">
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
-          <div className=" md:gap-8">
-            <div>
-              <h3 className="text-sm font-semibold text-warm-gray-200 tracking-wider uppercase">Legal</h3>
-              <ul className="mt-4 space-y-4">
-                {footerNavigation.legal.map((item) => (
-                  <li key={item.name}>
-                    <a href={item.href} className="text-base text-warm-gray-400 hover:text-warm-gray-300">
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          {/* </div> */}
-
         </div>
-
       </div>
-    </footer >
+    </footer>
   );
 }
